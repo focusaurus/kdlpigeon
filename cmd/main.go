038f16a0aae201e2bcfe6345efa613981d524fec
Peter Lyons <pete@peterlyons.com>
2023-09-19 18:29:53 -0400

    start pigeon parser with some scalar values
    Took null, true, false, numbers, and strings from JSON example

---
+++ cmd/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	kdl "github.com/focusaurus/kdlpigeon"
+)
+
+func main() {
+	nodes, err := kdl.ParseFile(os.Args[1])
+	if err != nil {
+		log.Fatalf("%s", err)
+	}
+	fmt.Printf("%+v\n", nodes)
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/focusaurus/kdlpigeon
+
+go 1.21.1
+
+require (
+	github.com/mna/pigeon v1.1.0 // indirect
+	golang.org/x/mod v0.12.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/tools v0.13.0 // indirect
+)
