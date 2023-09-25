d3d32d91cad86d6b419a22a29c86f13e58c0d553
Peter Lyons <pete@peterlyons.com>
2023-09-24 20:09:33 -0400

    unit test for simple values

--- parse_test.go
+++ parse_test.go
@@ -1,6 +1,7 @@
 package kdl
 
 import (
+	"sort"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -24,10 +25,49 @@ func emptyNode() Node {
 }
 
 func TestParse(t *testing.T) {
-	expected := emptyNode()
-	expected.Identifier = "foo"
-	result, err := Parse("test.kdl", []byte("foo"))
-	assert.NoError(t, err)
-	rootNode := mustRootNode(result)
-	assert.EqualValues(t, expected, rootNode)
+	tests := map[string]struct {
+		input    string
+		expected func() Node
+	}{
+		"just identifier": {
+			input: "foo",
+			expected: func() Node {
+				expected := emptyNode()
+				expected.Identifier = "foo"
+				return expected
+			},
+		},
+		"simple values": {
+			input: `foo 0 3.14 "hi" null`,
+			expected: func() Node {
+				expected := emptyNode()
+				expected.Identifier = "foo"
+				expected.Values = []Value{
+					parseValue(0.0),
+					parseValue(3.14),
+					parseValue("hi"),
+					parseValue(nil),
+				}
+				return expected
+			},
+		},
+	}
+
+	// To debug a particular test case first, add ! as a prefix to it's description.
+	// It will sort to the beginning and run first
+	keys := make([]string, 0)
+	for k := range tests {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	for _, desc := range keys {
+		t.Run(desc, func(t *testing.T) {
+			test, _ := tests[desc]
+			result, err := Parse("test.kdl", []byte(test.input))
+			assert.NoError(t, err)
+			rootNode := mustRootNode(result)
+			assert.EqualValues(t, test.expected(), rootNode)
+		})
+	}
 }
